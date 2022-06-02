1971f1f29bf7c790976945bbe5f5a185ab61d92f
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-02 08:23:29 +0000

    Listsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
   # URL「top」にアクセスすると、homesコントローラー内のtopアクションが実行される
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
