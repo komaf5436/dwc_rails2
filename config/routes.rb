21ccba8920f17fad97e4986b54de734229742094
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-05 16:53:02 +0000

    showのルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  # URL「top」にアクセスすると、homesコントローラー内のtopアクションが実行される
+  get '/top' => 'homes#top'
+  # 投稿フォームを表示するためのルーティング
   get 'lists/new'
   # データを追加するためのルーティングを追記
   post 'lists' => 'lists#create'
+  # 一覧表示するためのルーティング
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  # .../lists/1や.../lists/3に該当する
+  get 'lists' => 'lists#show'
   get 'lists/edit'
-  # URL「top」にアクセスすると、homesコントローラー内のtopアクションが実行される
-  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
