2f1bc1df0adb7d61f9f7ec8488572362bf87e9f3
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-06 06:55:38 +0000

    編集画面へのリンク

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   # 名前付きルートを設定することでわかりやすく
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
