e6237f33cdc8a1c891c7cbe407db9ee7f53597a2
komaf5436 <k.3624.t.aw@gmail.com>
2022-06-06 12:32:47 +0000

    Listモデルへバリデーション設定

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,10 @@
 class List < ApplicationRecord
   # 下記のように記述することで、Listモデルにtitleとbodyに加えて画像を扱うためのimageカラムが追記されたかのように扱うことが出来る。
   has_one_attached :image
+
+  # バリデーション設定
+  # presence: trueを記述することでデータが存在しなければならないという設定になる
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
